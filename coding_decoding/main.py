7cc7ff0d98e652692c9d8970de79135a298c8707
harish chaudhary <67159642+harish1907@users.noreply.github.com>
2022-05-30 20:59:54 +0530

    Add files via upload

---
+++ coding_decoding/main.py
@@ -0,0 +1,34 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def structure(text, shifts,namefn):
+    answer = ""
+    for i in text:
+        index = alphabet.index(i)
+        if namefn=='decoded':
+            index -= shifts
+        else:
+            index += shifts
+        answer += alphabet[index]    
+    print(f"The {namefn} text is {answer}")
+
+def encrypt( text, shifts ):
+    structure(text,shifts,'encoded')
+        
+def decrypt( text, shifts ):
+    structure(text,shifts,'decoded')
+
+while True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    if direction == 'encode' or direction == 'decode':
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
+        if direction == "encode":
+            encrypt( text, shift )
+        elif direction == "decode":
+            decrypt( text, shift )
+
+        yn = input('Do you want to continue(y/n)').lower()
+        if yn == 'n':
+            break
+    else:
+        print("You enter wrong direction please try again..")
